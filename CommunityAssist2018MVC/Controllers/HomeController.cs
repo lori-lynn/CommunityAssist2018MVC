2a8c61c88a16083c02021d29b7b272b0bb414229
lori-lynn <35235309+lori-lynn@users.noreply.github.com>
2018-01-24 11:21:39 -0800

    Add project files.

---
+++ CommunityAssist2018MVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssist2018MVC.Models;
+
+namespace CommunityAssist2018MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+            
+            return View(db.GrantTypes.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
