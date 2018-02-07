27f357246e90690d7d3b46f6f320640ff2d8607f
lori-lynn <35235309+lori-lynn@users.noreply.github.com>
2018-02-07 12:09:21 -0800

    registration

---
+++ CommunityAssist2018MVC/Controllers/RegistrationController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssist2018MVC.Models;
+
+namespace CommunityAssist2018MVC.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "PersonUsername, PersonLastName, PersonFirstName, PersonEmail, PersonPrimaryPhone, PersonAddressApt, PersonAddressStreet, PersonAddressCity, PersonAddressState, PersonAddressPostal, PersonPlainPassword")]NewPerson np)
+        {
+            int result = db.usp_Register(np.PersonUserName, np.PersonLastName, np.PersonFirstName, np.PersonEmail, np.PersonPrimaryPhone, np.PersonAddressApt, np.PersonAddressStreet, np.PersonAddressCity, np.PersonAddressState, np.PersonAddressPostal, np.PersonPlainPassword);
+
+            if (result != -1)
+            {
+                return RedirectToAction("Message");
+            }
+            return View();
+        }
+
+    }
+}
---
+++ CommunityAssist2018MVC/Models/NewPerson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommunityAssist2018MVC.Models
+{
+    public class NewPerson
+    {
+        public string PersonUserName
+        public string PersonLastName { get; set; }
+        public string PersonFirstName { get; set; }
+        public string PersonEmail { get; set; }
+        public string PersonPrimaryPhone { get; set; }
+        public string PersonAddressApt { get; set; }
+        public string PersonAddressStreet { get; set; }
+        public string PersonAddressCity { get; set; }
+        public string PersonAddressState { get; set; }
+        public string PersonAddressPostal { get; set; }
+        public string PersonPlainPassword { get; set; }
+    }
+}
